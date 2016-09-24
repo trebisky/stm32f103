7a1ec7273b8c91535b9a4eac0776d27b49305a05
Tom Trebisky <tom@mmto.org>
2016-09-24 13:13:04 -0700

    Fiddle a lot

--- blink1/blink.c
+++ blink1/blink.c
@@ -1,6 +1,78 @@
 /* blink.c */
 
+struct gpio {
+	unsigned long cr[2];
+	unsigned long idr;
+	unsigned long odr;
+	unsigned long bsrr;
+	unsigned long brr;
+	unsigned long lock;
+};
+
+#define GPIOA_BASE	(struct gpio *) 0x40010800
+#define GPIOB_BASE	(struct gpio *) 0x40010C00
+#define GPIOC_BASE	(struct gpio *) 0x40011000
+
+#define MODE_OUT_2	0x02	/* Output, 2 Mhz */
+
+#define CONF_GP_UD	0x0	/* Pull up/down */
+#define CONF_GP_OD	0x4	/* Open drain */
+
+void
+delay ( void )
+{
+	volatile int count = 1000 * 1000;
+
+	while ( count-- )
+	    ;
+}
+
+struct gpio *gp;
+unsigned long on_mask;
+unsigned long off_mask;
+
+void
+led_init ( int bit )
+{
+	int conf;
+	int shift;
+
+	gp = GPIOC_BASE;
+
+	shift = (bit - 8) * 4;
+	conf = gp->cr[1] & ~(0xf<<shift);
+	conf |= (MODE_OUT_2|CONF_GP_OD) << shift;
+	gp->cr[1] = conf;
+
+	on_mask = 1 << bit;
+	off_mask = 1 << (bit+16);
+}
+
+void
+led_on ( void )
+{
+	gp->bsrr = on_mask;
+}
+
+void
+led_off ( void )
+{
+	gp->bsrr = off_mask;
+}
+
+#define PC13	13
+
 void
 startup ( void )
 {
+	led_init ( PC13 );
+
+	for ( ;; ) {
+	    led_on ();
+	    delay ();
+	    led_off ();
+	    delay ();
+	}
 }
+
+/* THE END */
--- blink1/locore.s
+++ blink1/locore.s
@@ -3,32 +3,32 @@
  * Tom Trebisky  9-24-2016
  */
 
-.word   0x10008000  /* stack top address */
+.word   0x20005000  /* stack top address */
 .word   _reset      /* 1 Reset */
-.word   hang        /* 2 NMI */
-.word   hang        /* 3 Hard Fault */
-.word   hang        /* 4 MM Fault */
-.word   hang        /* 5 Bus Fault */
-.word   hang        /* 6 Usage Fault */
-.word   hang        /* 7 RESERVED */
-.word   hang        /* 8 RESERVED */
-.word   hang        /* 9 RESERVED*/
-.word   hang        /* 10 RESERVED */
-.word   hang        /* 11 SV call */
-.word   hang        /* 12 Debug reserved */
-.word   hang        /* 13 RESERVED */
-.word   hang        /* 14 PendSV */
-.word   hang        /* 15 SysTick */
-.word   hang        /* 16 IRQ0 */
-.word   hang        /* 17 IRQ1 */
-.word   hang        /* 18 IRQ2 */
-.word   hang        /* 19 ...   */
+.word   spin        /* 2 NMI */
+.word   spin        /* 3 Hard Fault */
+.word   spin        /* 4 MM Fault */
+.word   spin        /* 5 Bus Fault */
+.word   spin        /* 6 Usage Fault */
+.word   spin        /* 7 RESERVED */
+.word   spin        /* 8 RESERVED */
+.word   spin        /* 9 RESERVED*/
+.word   spin        /* 10 RESERVED */
+.word   spin        /* 11 SV call */
+.word   spin        /* 12 Debug reserved */
+.word   spin        /* 13 RESERVED */
+.word   spin        /* 14 PendSV */
+.word   spin        /* 15 SysTick */
+.word   spin        /* 16 IRQ0 */
+.word   spin        /* 17 IRQ1 */
+.word   spin        /* 18 IRQ2 */
+.word   spin        /* 19 ...   */
 		    /* On to IRQ67 */
 
-hang:   b .
+spin:   b spin
 
-.globl _reset
 _reset:
     bl startup
+    b .
 
 /* THE END */
